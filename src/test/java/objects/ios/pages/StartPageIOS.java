76447df0410950b0fd7271e052febfa2e098fe04
Смотров Илья Алексеевич <ISmotrov@alfabank.ru>
2023-01-23 19:31:00 +0300

    Translate steps name to English

--- src/test/java/objects/ios/pages/StartPageIOS.java
+++ src/test/java/objects/ios/pages/StartPageIOS.java
@@ -1,17 +1,20 @@
 package objects.ios.pages;
 
+import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 import objects.android.pages.StartPage;
 
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$x;
 
 public class StartPageIOS extends StartPage {
 
+    protected final SelenideElement welcomeButtons = $x("//*[contains(@resource-id,'welcome_buttons_on_ios')]");
+
     @Override
-    @Step("Экран 'Корзина' открыт")
+    @Step("Initial screen should be opened")
     public StartPage shouldBeOpen() {
         welcomeButtons.shouldBe(visible);
         return this;
     }
-
 }
--- src/test/java/tests/authorization/AuthorizationTest.java
+++ src/test/java/tests/authorization/AuthorizationTest.java
@@ -13,10 +13,10 @@ import static helpers.factories.ObjectProvider.instance;
 import static io.qameta.allure.Allure.step;
 
 @Feature("Авторизация")
-@Owner("Mvideo Admin")
+@Owner("Admin")
 public class AuthorizationTest extends BaseTest {
 
-    @Test(description = "Проверка успешного перехода к регистрации")
+    @Test(description = "Check user can proceed to registration screen")
     @AllureId("2")
     @Tag("FM03")
     public void openRegistration() {
@@ -30,42 +30,41 @@ public class AuthorizationTest extends BaseTest {
                 .shouldBeOpen();
     }
 
-    @Test(description = "Проверка доступности кнопки регистрации")
+    @Test(description = "Check registration button is available")
     @AllureId("3")
-    @Feature("Авторизация")
+    @Feature("Autorization")
     public void createAccountValidation() {
         StartPage cartPage = instance(StartPage.class);
         RegistrationPage registrationPage = instance(RegistrationPage.class);
-        step("Стартовый экран открыт", () -> {
+        step("Initial screen should be opened", () -> {
             cartPage
                     .shouldBeOpen();
         });
-        step("Нажать на Продолжить с другими опциями", () -> {
+        step("Press on 'Continue with other options' button", () -> {
             cartPage
                     .clickMoreOptions();
         });
-        step("Нажать на опцию Зарегистрироваться с помощью email", () -> {
+        step("Press on 'Register with email' button", () -> {
             cartPage
                     .clickEmailOption();
         });
-        step("Экран регистрации открыт", () -> {
+        step("Registration screen should be opened", () -> {
             registrationPage
                     .shouldBeOpen();
         });
-        step("Ввести значение test@test.test в поле email", () -> {
+        step("Enter value 'test@test.test' to the 'email' field", () -> {
             registrationPage
                     .setEmailField("test@test.test");
         });
-        step("Ввести значение 12345678 в поле пароль", () -> {
+        step("Enter value '12345678' to the password field", () -> {
             registrationPage
                     .setPasswordField("12345678");
 
         });
-        step("Кнопка Создать аккаунт активна", () -> {
+        step("'Create Account' button should be active", () -> {
             registrationPage
                     .createAccountEnabled();
         });
     }
-
 
 }
